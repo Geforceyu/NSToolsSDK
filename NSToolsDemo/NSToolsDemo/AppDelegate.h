50be9241d0778caf950422bfb4adef5000f8962a
Nemo <lm4413000@qq.com>
2014-01-06 17:51:06 +0800

    nemo
    nemo

---
+++ NSToolsDemo/NSToolsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSToolsDemo
+//
+//  Created by Nemo on 14-1-6.
+//  Copyright (c) 2014年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
