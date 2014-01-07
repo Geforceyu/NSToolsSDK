cc023f4b95853e0fa50ccc78a7546f4009ac9bbf
Geforceyu <Geforceyu@gmail.com>
2014-01-07 09:56:54 +0800

    1
    1

---
+++ NSToolsDemo/NSToolsSDK/WidgetTools/1.h
@@ -0,0 +1,13 @@
+//
+//  1.h
+//  NSToolsDemo
+//
+//  Created by Ych on 14-1-7.
+//  Copyright (c) 2014年 Nemo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _ : NSObject
+
+@end
---
+++ NSToolsDemo/NSToolsSDK/WidgetTools/1.m
@@ -0,0 +1,13 @@
+//
+//  1.m
+//  NSToolsDemo
+//
+//  Created by Ych on 14-1-7.
+//  Copyright (c) 2014年 Nemo. All rights reserved.
+//
+
+#import "1.h"
+
+@implementation _
+
+@end
